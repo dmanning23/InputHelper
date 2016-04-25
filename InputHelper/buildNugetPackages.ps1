9cac56d6db0bdf6a2f0fa6467e2e6cd51e62b856
Dan Manning <dmanning23@hotmail.com>
2016-04-24 19:58:51 -0500

    moved into pcl, added winuni

--- InputHelper/buildNugetPackages.ps1
+++ InputHelper/buildNugetPackages.ps1
@@ -1,3 +1,2 @@
-nuget pack .\InputHelper.Android\InputHelper.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\InputHelper.iOS\InputHelper.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\InputHelper.DesktopGL\InputHelper.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\InputHelper.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
