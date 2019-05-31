6b90374119ca38045d0f593be7240272450a2734
Dan Manning <dmanning23@hotmail.com>
2019-05-31 16:10:52 -0500

    Added Flickable and Pinchable behaviors

---
+++ InputHelper/InputHelper.SharedProject/IFlickable.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace InputHelper
+{
+	/// <summary>
+	/// This is an item that can be flicked
+	/// </summary>
+	public interface IFlickable
+	{
+		bool Flickable { get; set; }
+
+		bool IsFlicked { get; set; }
+
+		event EventHandler<FlickEventArgs> OnFlick;
+
+		bool CheckFlick(FlickEventArgs flick);
+	}
+}
---
+++ InputHelper/InputHelper.SharedProject/IPinchable.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace InputHelper
+{
+	/// <summary>
+	/// This is an item that can be pinched.
+	/// </summary>
+	public interface IPinchable
+	{
+		bool Pinchable { get; set; }
+
+		bool IsPinched { get; set; }
+
+		event EventHandler<PinchEventArgs> OnPinch;
+
+		bool CheckPinch(PinchEventArgs pinch);
+	}
+}
